5ca1c3de9ff5a05238ce89ef21525f0581af4af4
Alex Milroy <amilroy@gmail.com>
2020-11-05 12:38:26 -0800

    got pages running

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import NavTabs from "./components/NavTabs";
-import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import Discover from "./components/pages/Discover";
 import Search from "./components/pages/Search";
@@ -11,7 +10,7 @@ function App() {
     <Router>
       <div>
         <NavTabs />
-        <Route exact path="/" component={Home} />
+        <Route exact path="/" component={About} />
         <Route exact path="/about" component={About} />
         <Route exact path="/discover" component={Discover} />
         <Route path="/search" component={Search} />
--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -9,11 +9,11 @@ function NavTabs() {
 
   return (
     <ul className="nav nav-tabs">
-      <li className="nav-item">
+      {/* <li className="nav-item">
         <Link to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
-          Home
+          About
         </Link>
-      </li>
+      </li> */}
       <li className="nav-item">
         <Link
           to="/about"
@@ -24,26 +24,18 @@ function NavTabs() {
       </li>
       <li className="nav-item">
         <Link
-          to="/blog"
-          className={location.pathname === "/blog" ? "nav-link active" : "nav-link"}
+          to="/discover"
+          className={location.pathname === "/discover" ? "nav-link active" : "nav-link"}
         >
-          Blog
+          Discover
         </Link>
       </li>
       <li className="nav-item">
         <Link
-          to="/contact"
-          className={location.pathname === "/contact" ? "nav-link active" : "nav-link"}
+          to="/search"
+          className={location.pathname === "/search" ? "nav-link active" : "nav-link"}
         >
-          Contact
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/contact/learn"
-          className={location.pathname === "/contact/learn" ? "nav-link active" : "nav-link"}
-        >
-          Learn
+          Search
         </Link>
       </li>
     </ul>
